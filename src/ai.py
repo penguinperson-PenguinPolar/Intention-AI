1076143af1659059388738909ff63ab1e89f562c
Lars <penguinperson.pp@gmail.com>
2022-09-02 18:05:28 -0400

    Update ai.py

--- src/ai.py
+++ src/ai.py
@@ -1,6 +1,8 @@
 # This is a very simple AI (Artificial Intelligence) that can tell the difference between a circle and a rectangle.
 
 from PIL import Image
+import pyttsx3
+engine = pyttsx3.init()
 
 image = Image.open("ai_circle1.png")
 cir1img = image.load()
@@ -60,32 +62,44 @@ def train():
                     0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
                     0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]            
 
-    circle1 = False
-    circle2 = False
-    rect1 = False
-    rect2 = False
-
     while True:
+        circle1 = False
+        circle2 = False
+        rect1 = False
+        rect2 = False
         bias += 1
         if check(circle1_data, bias, weights) == True:circle1 = True
         if check(circle2_data, bias, weights) == True:circle2 = True
         if check(rect1_data, bias, weights) == False:rect1 = True
         if check(rect2_data, bias, weights) == False:rect2 = True
-        if circle1 and circle2 and rect1 and rect2:
-            return bias,weights
+        if circle1 == True and circle2 == True and rect1 == True and rect2 == True:return bias,weights
         else:
-            if circle1 == False:
-                for i in range(len(circle1_data)):
-                    weights[i] += circle1_data[i]
-            if circle2 == False:
-                for i in range(len(circle2_data)):
-                    weights[i] += circle2_data[i]
-            if rect1 == False:
-                for i in range(len(rect1_data)):
-                    weights[i] += rect1_data[i]
-            if rect2 == False:
-                for i in range(len(rect2_data)):
-                    weights[i] += rect2_data[i]
+            if circle1 == False or circle2 == False:
+                if circle1 == False:
+                    for i in range(len(circle1_data)):
+                        weights[i] += circle1_data[i]
+                if circle2 == False:
+                    for i in range(len(circle2_data)):
+                        weights[i] += circle2_data[i]
+                if rect1 == False:
+                    for i in range(len(rect1_data)):
+                        weights[i] += rect1_data[i]
+                if rect2 == False:
+                    for i in range(len(rect2_data)):
+                        weights[i] += rect2_data[i]
+            if rect1 == False or rect2 == False:
+                if circle1 == False:
+                    for i in range(len(circle1_data)):
+                        weights[i] -= circle1_data[i]
+                if circle2 == False:
+                    for i in range(len(circle2_data)):
+                        weights[i] -= circle2_data[i]
+                if rect1 == False:
+                    for i in range(len(rect1_data)):
+                        weights[i] -= rect1_data[i]
+                if rect2 == False:
+                    for i in range(len(rect2_data)):
+                        weights[i] -= rect2_data[i]
 
 bias,weights = train()
 print("Training Ended")
